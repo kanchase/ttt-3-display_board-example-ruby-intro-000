4bbcd4c9c1080a0e65632036a958b8bf6e32961e
Mya McElroy <mya.mcelroy@gmail.com>
2018-06-01 21:39:53 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 def display_board
   puts "A Tic Tac Toe Board"
-  row1 = " ",|," ",|," "
-  row2 = " ",|," ",|," "
-  row3 = " ",|," ",|," "
+  row1 = " ",|" "|," "
+  row2 = " ",|" "|," "
+  row3 = " ",|" "|," "
   print row1
   print row2
   print row3
